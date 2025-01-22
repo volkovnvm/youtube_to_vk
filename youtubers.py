e0ecb9e8ac79a9e6b6a5b93e99dd132d0f271fbe
VolkovN <161017258+volkovnvm@users.noreply.github.com>
2025-01-22 21:16:04 +0500

    Update youtubers.py

--- youtubers.py
+++ youtubers.py
@@ -1,15 +1,16 @@
 class Youtuber:
-    def __init__(self, name, group_id, pl_id):
+    def __init__(self, name, group_id, pl_id=None):
         self.name = name #имя ютубера после @
         self.group_id = group_id 
         self.pl_id = pl_id #номер плейлиста в группе вк
-
+        
+    def __str__(self):
+        return self.name
+        
 youtubers_list = [
     Youtuber(name, group_id, pl_id),   #короткая ссылка на ютубера в поле name, айди группы в вк в поле group_id, айди плейлиста в поле pl_id
-    Youtuber(name, group_id, pl_id),
-    Youtuber(name, group_id, pl_id),
-    Youtuber(name, group_id, pl_id),
-    Youtuber(name, group_id, pl_id),
-    Youtuber(name, group_id, pl_id)
+    Youtuber(name, group_id, pl_id),   
+    Youtuber(name, group_id, pl_id),   
+    Youtuber(name, group_id, pl_id),   
     # и так далее
 ]
