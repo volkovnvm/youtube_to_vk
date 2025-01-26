48ef27266acd3ae5126dca6f95db00f25a35ec79
VolkovN <161017258+volkovnvm@users.noreply.github.com>
2025-01-26 19:18:45 +0500

    Update main.py
    добавил автоматическое определение местонахождения скрипта, необходимое для загрузки обложки видео.
    разделил функционал по функциям get_url() и download_video()

--- main.py
+++ main.py
@@ -11,82 +11,88 @@ VK_TOKEN = os.getenv(TOKEN_VK) #также токен можно указать 
 vk_session = vk_api.VkApi(token=VK_TOKEN)
 upload = vk_api.VkUpload(vk_session)
 vk = vk_session.get_api()
+script_dir = os.path.dirname(os.path.abspath(__main__))
 
 # user_id = заменить на ваш айди в вк  
 user_id = os.getenv(USER_ID) #аналогично можно указать напрямую в файле как и токен
 
-while True:
-    try:
-        for youtuber in youtubers_list:
-            print("получаем хтмл")
+def get_url(youtuber) -> str:
             html = requests.get(f"https://www.youtube.com/@{youtuber}/videos").text
             print("проверяем последнее видео")
             URL = (
                 "https://www.youtube.com/watch?v="
                 + re.search('(?<="videoId":").*?(?=")', html).group()
             )
+            return URL
 
+ydl_opts = {
+    "format": "bestvideo+bestaudio/best",
+    "concurrent-fragments": "4",
+    "outtmpl": "video.%(ext)s",
+    "ignoreerrors": True,
+    "no-overwrites": True,
+    "continue": True,
+    "writethumbnail": True,
+    "embedthumbnail": True,
+    "convert-thumbnails": "jpg",
+    "postprocessors": [
+        {
+            "format": "jpg",
+            "key": "FFmpegThumbnailsConvertor",
+            "when": "before_dl",
+        }
+    ],
+}
+
+def download_video(URL):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(URL, download=True)
+            video_channel = info_dict.get("channel", None)
+            video_title = info_dict.get("title", None)
+            video_format = info_dict.get("ext", None)
+            video_name = f"[{video_channel}] {video_title}"
+            file_name = f"video.{video_format}"
+        except:
+            vk.messages.send(
+                user_id=user_id,
+                random_id=get_random_id(),
+                message=f"ОШИБКА! ВИДЕО {video_name} СКАЧАТЬ НЕ УДАЛОСЬ",
+            )
+    print("загрузка видео в вк")
+    response = upload.video(
+        video_file=file_name,
+        name=video_name,
+        group_id=youtuber.group_id,
+        album_id=youtuber.pl_id,
+        wallpost="1",
+    )
+    print("загрузка обложки")
+    video_id = response.get("video_id")
+    response = upload.thumb_video(
+        photo_path=script_dir+"/video.jpg",
+        owner_id=f"-{group_id}",
+        video_id=video_id,
+    )
+    vk.messages.send(
+        user_id=user_id,
+        random_id=get_random_id(),
+        message=f"Видео {video_name} успешно загружено в группу",
+    )
+    file.write(URL + "\n")
+    os.remove(file_name)
+    os.remove("video.jpg")
+
+
+
+while True:
+    try:
+        for youtuber in youtubers_list:
+            URL = get_url(youtuber)
             with open("downloaded.txt", "r+") as file:
                 content = file.read()
-
                 if URL not in content:
-                    ydl_opts = {
-                        "format": "bestvideo+bestaudio/best",
-                        "concurrent-fragments": "4",
-                        "outtmpl": "video.%(ext)s",
-                        "ignoreerrors": True,
-                        "no-overwrites": True,
-                        "continue": True,
-                        "writethumbnail": True,
-                        "embedthumbnail": True,
-                        "convert-thumbnails": "jpg",
-                        "postprocessors": [
-                            {
-                                "format": "jpg",
-                                "key": "FFmpegThumbnailsConvertor",
-                                "when": "before_dl",
-                            }
-                        ],
-                    }
-
-                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                        try:
-                            info_dict = ydl.extract_info(URL, download=True)
-                            video_channel = info_dict.get("channel", None)
-                            video_title = info_dict.get("title", None)
-                            video_format = info_dict.get("ext", None)
-                            video_name = f"[{video_channel}] {video_title}"
-                            file_name = f"video.{video_format}"
-                        except:
-                            vk.messages.send(
-                                user_id=user_id,
-                                random_id=get_random_id(),
-                                message=f"ОШИБКА! ВИДЕО {video_name} СКАЧАТЬ НЕ УДАЛОСЬ",
-                            )
-                            break
-                    print("загрузка видео в вк")
-                    response = upload.video(
-                        video_file=file_name,
-                        name=video_name,
-                        group_id=youtuber.group_id,
-                        album_id=youtuber.pl_id,
-                        wallpost="1",
-                    )
-                    print("загрузка обложки")
-                    video_id = response.get("video_id")
-                    response = upload.thumb_video(
-                        photo_path="/root/video.jpg", #здесь нужно указать прямой путь до папки с программой в формате "/путь/к/файлу/video.jpg"
-                        owner_id=f"-{group_id}",
-                        video_id=video_id,
-                    )
-                    vk.messages.send(
-                        user_id=user_id,
-                        random_id=get_random_id(),
-                        message=f"Видео {video_name} успешно загружено в группу",
-                    )
-                    file.write(URL + "\n")
-                    os.remove(file_name)
-                    os.remove("video.jpg")
+                    download_video(URL)
         print("перерыв 10 минут")
         time.sleep(10 * 60)
     except:
